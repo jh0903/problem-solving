b7ee1e40ba8de79b8d5587006c15b6dfbbf3ded1
Joohee <69308773+jh0903@users.noreply.github.com>
2023-09-08 15:50:28 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+
+priority_queue<int> pq_desc;
+priority_queue<int, vector<int>, greater<int>> pq_asc;
+map<string, int> mp;
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+    answer.push_back(0); answer.push_back(0);
+    for(string o : operations){
+        if(o[0] == 'I'){
+            int num = stoi(o.substr(2, sizeof(o)));
+            pq_asc.push(num);
+            pq_desc.push(num);
+            mp[to_string(num)] = 1;
+        }else {
+            if(o[2] == '1'){
+                while(!pq_desc.empty()){
+                    int mx = pq_desc.top(); pq_desc.pop();
+                    if(mp[to_string(mx)]){
+                        mp[to_string(mx)] = 0;
+                        break;
+                    }
+                }
+            }
+            else {
+                while(!pq_asc.empty()){
+                    int mi = pq_asc.top(); pq_asc.pop();
+                    if(mp[to_string(mi)]){
+                        mp[to_string(mi)] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    while(!pq_desc.empty()){
+        int cur = pq_desc.top();
+        if(mp[to_string(cur)]) {
+            answer[0] = cur;
+            break;
+        }
+        else pq_desc.pop();
+    }
+    while(!pq_asc.empty()){
+        int cur = pq_asc.top();
+        if(mp[to_string(cur)]) {
+            answer[1] = cur;
+            break;
+        }
+        else pq_asc.pop();
+    }
+    return answer;
+}
