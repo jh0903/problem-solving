676909283145d65672dd7d4dcbee7a4057396484
Joohee <69308773+jh0903@users.noreply.github.com>
2023-09-22 16:54:43 +0900

    [Gold IV] Title: 도서관, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.cc
+++ 백준/Gold/1461. 도서관/도서관.cc
@@ -1,54 +1,29 @@
 #include<iostream>
-#include<queue>
 #include<algorithm>
 #include<vector>
+#include<cmath>
 using namespace std;
 
 int N, M, ans;
-vector<int> neg, pos;
+vector<int> num;
 
 int main(void) {
 	ios::ios_base::sync_with_stdio(false); cin.tie(0);
 	cin >> N >> M;
+	int pivot = 0;
 	for (int x, i = 0;i < N;i++) {
 		cin >> x;
-		if (x < 0) neg.push_back(x);
-		else pos.push_back(x);
+		if (x < 0) pivot++;
+		num.push_back(x);
 	}
-	sort(neg.begin(), neg.end());
-	sort(pos.begin(), pos.end());
-	if (!neg.empty() && pos.empty()) {
-		ans += -neg[0];
-		for (int i = M;i < neg.size();i += M) {
-			ans += -neg[i] * 2;
-		}
+	sort(num.begin(), num.end());
+	for (int i = 0;i < pivot;i += M) {
+		ans += abs(num[i]) * 2;
 	}
-	else if (neg.empty() && !pos.empty()) {
-		ans += pos[pos.size() - 1];
-		for (int i = pos.size() - 1 - M;i >= 0;i-=M) {
-			ans += pos[i] * 2;
-		}
+	for (int i = N - 1;i >= pivot;i -= M) {
+		ans += abs(num[i]) * 2;
 	}
-	else {
-		if (-neg[0] > pos[pos.size() - 1]) {
-			ans += -neg[0];
-			for (int i = M;i < neg.size();i += M) {
-				ans += -neg[i] * 2;
-			}
-			for (int i = pos.size() - 1;i >= 0;i -= M) {
-				ans += pos[i] * 2;
-			}
-		}
-		else {
-			ans += pos[pos.size() - 1];
-			for (int i = 0;i < neg.size();i += M) {
-				ans += -neg[i] * 2;
-			}
-			for (int i = pos.size() - 1 - M;i >= 0;i -= M) {
-				ans += pos[i] * 2;
-			}
-		}
-	}
+	ans -= max(abs(num[0]), abs(num[N - 1]));
 	cout << ans;
 	return 0;
 }
