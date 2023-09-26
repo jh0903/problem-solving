f04eb6dd7431085e64cf0478ce2eb417d5ef1ef0
Joohee <69308773+jh0903@users.noreply.github.com>
2023-09-26 17:32:21 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cpp
@@ -4,19 +4,12 @@
 #include <iostream>
 using namespace std;
 
-int sum[10001];
-
 int solution(vector<int> citations) {
     int answer = 0;
-    sort(citations.begin(), citations.end());
-    for(int citation: citations){
-        sum[citation]++;
-    }
-    for(int i=9999;i>=0;i--){
-        sum[i] += sum[i+1];
-        if(i <= sum[i]){
-            answer = i;
-            break;
+    sort(citations.begin(), citations.end(), greater<int>());
+    for(int i=0;i<citations.size();i++){
+        if(citations[i] >= i + 1){
+            answer = i+1;
         }
     }
     
